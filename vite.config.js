799f8eb7cf1418481abab9c0f1a6d5d79f28bc2c
agnesb942-hash <agnesb942@gmail.com>
2026-03-01 23:50:39 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: false,
+  esbuild: {
+    include: /\.(ts|js)$/,
+    exclude: /\.jsx$/,
+  },
 })
